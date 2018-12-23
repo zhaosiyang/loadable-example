019ec3258d794920965ebc5f5e7962e7c70b6847
赵思阳 <zhaosiyang@s-MacBook-puro.local>
2018-12-23 17:25:11 -0500

    use mock API

--- src/core/news.effects.ts
+++ src/core/news.effects.ts
@@ -1,19 +1,31 @@
 import {Injectable} from '@angular/core';
 import {Actions, Effect, ofType} from '@ngrx/effects';
-import {catchError, map, switchMap} from 'rxjs/operators';
+import {catchError, delay, map, switchMap} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
-import {of} from 'rxjs';
+import {Observable, of, pipe} from 'rxjs';
 import {LoadNewsError, LoadNewsSuccess, NewsActionsTypes} from './news.actions';
+
+function mockApiResponse(): Observable<{ todaysNews: string[] }> {
+  return of({
+    todaysNews: ['news1', 'news2', 'news3', 'news4']
+  }).pipe(
+    delay(1000)
+  );
+}
 
 @Injectable()
 export class NewsEffects {
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  constructor(private actions$: Actions, private http: HttpClient) {
+  }
 
   @Effect()
   loadNews = this.actions$.pipe(
     ofType(NewsActionsTypes.Load),
-    switchMap(action => this.http.get('https://demo9695495.mockable.io')),
+    switchMap(action => {
+      // return this.http.get('some url');
+      return mockApiResponse();
+    }),
     map((response: any) => new LoadNewsSuccess(response.todaysNews)),
     catchError(error => of(new LoadNewsError(error)))
   );
