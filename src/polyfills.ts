144cc41b7f5f41c8d164f013e3544f6afeefe0de
赵思阳 <zhaosiyang@s-MacBook-puro.local>
2018-12-23 16:50:42 -0500

    resolve stackblitz import issue

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -33,6 +33,7 @@
 // import 'core-js/es6/map';
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
+import 'core-js/es7/reflect';
 
 /**
  * If the application will be indexed by Google Search, the following is required.
